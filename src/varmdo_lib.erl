93817b86d78e0c34ccc2f33b119d92b25f05901d
joq62 <joakim.leche@gmail.com>
2020-08-12 22:35:27 +0200

    update

--- src/varmdo_lib.erl
+++ src/varmdo_lib.erl
@@ -32,8 +32,22 @@
 %% --------------------------------------------------------------------
 info()->
     [TellstickNode|_]=sd_service:fetch_service("tellstick_service"),
-    R=rpc:call(TellstickNode,tellstick_service,get_all_info,[]),
-    {reply,"info_ret",R}.
+    L=rpc:call(TellstickNode,tellstick_service,get_all_info,[]),
+    {device_info,DeviceInfo}=lists:keyfind(device_info,1,L),
+    {sensor_info,SensorInfo}=lists:keyfind(sensor_info,1,L),
+    Dev1=[Id++"="++Value++"; "||{_,Id,Value}<-DeviceInfo,
+		     Id/="ej anvand"],
+    Sens1=lists:append([Id++"="++Temp++"; "||{Id,Temp}<-SensorInfo],
+		       [Id++"="++Temp++"; "||{Id,Temp,_}<-SensorInfo]),
+   
+   % Dev2=unicode:characters_to_list(Dev1,unicode),
+   % Sens2=unicode:characters_to_list(Sens1,unicode),
+   % io:format("~p~n",[Dev2]),
+   % io:format("~p~n",[Sens2]),
+    Subject="Info",
+    Msg=unicode:characters_to_list([Dev1,Sens1],unicode),
+    %  io:format("~p~n",[Msg]),
+    {reply,Subject,Msg}.
 %% --------------------------------------------------------------------
 %% Function: 
 %% Description:
